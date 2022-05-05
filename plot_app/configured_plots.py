cf1457e0b0c17aec81456d7abb45ca9d312414c7
Herman Øie Kolden <herman@samfundet.no>
2022-05-06 00:07:35 +0200

    Update thrust interference scatter
    Always show mag norm from 0 to 1, and fix a bug in the legend

--- plot_app/configured_plots.py
+++ plot_app/configured_plots.py
@@ -812,7 +812,6 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
     thrust_timestamps = data_plot.dataset.data['timestamp']
     thrust_values = data_plot.dataset.data['control[3]']
 
-    min_mag_norm = np.inf
     max_mag_norm = 0
     for topic_instance in range(2):
         data_plot.change_dataset(magnetometer_ga_topic, topic_instance=topic_instance)
@@ -826,13 +825,10 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
         mag_timestamps = data_plot.dataset.data['timestamp']
         thrust_values_interp = np.interp(mag_timestamps, thrust_timestamps, thrust_values)
 
-        min_mag_norm = np.min([min_mag_norm, np.min(mag_norm_values)])
-        max_mag_norm = np.max([max_mag_norm, np.max(mag_norm_values)])
-
-        filter_idxs = thrust_values_interp > min_thrust
+        filter_idxs = thrust_values_interp >= min_thrust
         mag_norm_filtered = mag_norm_values[filter_idxs]
         thrust_filtered = thrust_values_interp[filter_idxs]
-        cavg = np.average(mag_norm_filtered)
+
         c1, c0 = np.polyfit(thrust_filtered, mag_norm_filtered, 1)
         fit_func = np.poly1d((c1, c0))
 
@@ -842,16 +838,15 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
                 legend_label=f'Magnetometer {topic_instance}')
 
         polyfit_xs = np.linspace(0, 1, 100)
+        polyfit_ys = fit_func(polyfit_xs)
+        max_mag_norm = np.max([max_mag_norm, np.max(polyfit_ys)])
         p.line(polyfit_xs, fit_func(polyfit_xs),
                 color=colors8[topic_instance],
-                legend_label=f'Fit {topic_instance}: y={c0:.3f} (1 + {c1:.3f}x)')
+                legend_label=f'Fit {topic_instance}: y={c0:.3f} (1 + {c1/c0:.3f}x)')
 
 
-        p = data_plot.bokeh_plot
-
-    dy = max_mag_norm-min_mag_norm
     p.x_range = Range1d(start=min_thrust, end=1)
-    p.y_range = Range1d(start=min_mag_norm-dy*0.1, end=max_mag_norm+dy*0.1)
+    p.y_range = Range1d(start=0, end=max_mag_norm*1.1)
 
     if data_plot.finalize() is not None: plots.append(data_plot)
 
