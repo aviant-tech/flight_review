aviant-tech/flight_review

commit 9d1f4cde15d503e76536832c9b05e3cc8d8e4764

Use DatabaseULog in plot_app

plot_app/plotting.py
@@ -451,7 +451,7 @@ class DataPlot:
     """
 
 
-    def __init__(self, data, config, data_name, x_axis_label=None,
+    def __init__(self, ulog, config, data_name, x_axis_label=None,
                  y_axis_label=None, title=None, plot_height='normal',
                  y_range=None, y_start=None, changed_params=None,
                  topic_instance=0, x_range=None):
@@ -460,7 +460,7 @@ class DataPlot:
         self._previous_success = False
         self._param_change_label = None
 
-        self._data = data
+        self._ulog = ulog
         self._config = config
         self._plot_height_name = plot_height
         self._data_name = data_name
@@ -482,8 +482,7 @@ class DataPlot:
                     plot_parameter_changes(self._p, self.plot_height,
                                            changed_params)
 
-            self._cur_dataset = [elem for elem in data
-                                 if elem.name == data_name and elem.multi_id == topic_instance][0]
+            self._cur_dataset = ulog.get_dataset(data_name)
 
             if y_start is not None:
                 # make sure y axis starts at y_start. We do it by adding an invisible circle
@@ -536,8 +535,7 @@ class DataPlot:
         if not self._had_error: self._previous_success = True
         self._had_error = False
         try:
-            self._cur_dataset = [elem for elem in self._data
-                                 if elem.name == data_name and elem.multi_id == topic_instance][0]
+            self._cur_dataset = self._ulog.get_dataset(data_name, multi_instance=topic_instance)
         except (KeyError, IndexError, ValueError) as error:
             print(type(error), "("+self._data_name+"):", error)
             self._had_error = True
@@ -763,11 +761,11 @@ class DataPlot2D(DataPlot):
     """
 
 
-    def __init__(self, data, config, data_name, x_axis_label=None,
+    def __init__(self, ulog, config, data_name, x_axis_label=None,
                  y_axis_label=None, title=None, plot_height='normal',
                  equal_aspect=True):
 
-        super(DataPlot2D, self).__init__(data, config, data_name,
+        super(DataPlot2D, self).__init__(ulog, config, data_name,
                                          x_axis_label=x_axis_label,
                                          y_axis_label=y_axis_label,
                                          title=title, plot_height=plot_height)
@@ -827,11 +825,11 @@ class DataPlotSpec(DataPlot):
     A spectrogram plot is only added to the plotting page if the sampling frequency of the dataset is higher than 100Hz.
     """
 
-    def __init__(self, data, config, data_name, x_axis_label=None,
+    def __init__(self, ulog, config, data_name, x_axis_label=None,
                  y_axis_label=None, title=None, plot_height='small',
                  x_range=None, y_range=None, topic_instance=0):
 
-        super(DataPlotSpec, self).__init__(data, config, data_name, x_axis_label=x_axis_label,
+        super(DataPlotSpec, self).__init__(ulog, config, data_name, x_axis_label=x_axis_label,
                                            y_axis_label=y_axis_label, title=title, plot_height=plot_height,
                                            x_range=x_range, y_range=y_range, topic_instance=topic_instance)
 
@@ -929,11 +927,11 @@ class DataPlotFFT(DataPlot):
     the dataset is higher than 100Hz.
     """
 
-    def __init__(self, data, config, data_name,
+    def __init__(self, ulog, config, data_name,
                  title=None, plot_height='small',
                  x_range=None, y_range=None, topic_instance=0):
 
-        super(DataPlotFFT, self).__init__(data, config, data_name, x_axis_label='Hz',
+        super(DataPlotFFT, self).__init__(ulog, config, data_name, x_axis_label='Hz',
                                           y_axis_label='Amplitude * 1000', title=title, plot_height=plot_height,
                                           x_range=x_range, y_range=y_range, topic_instance=topic_instance)
         self._use_time_formatter = False

plot_app/main.py
@@ -19,6 +19,7 @@ from db_entry import *
 from configured_plots import generate_plots
 from pid_analysis_plots import get_pid_analysis_plots
 from statistics_plots import StatisticsPlots
+from pyulog.db import DatabaseULog
 
 #pylint: disable=invalid-name, redefined-outer-name
 
@@ -98,7 +99,6 @@ else:
     log_id = ''
 
     try:
-
         if GET_arguments is not None and 'log' in GET_arguments:
             log_args = GET_arguments['log']
             if len(log_args) == 1:
@@ -108,17 +108,26 @@ else:
                 print('GET[log]={}'.format(log_id))
                 ulog_file_name = get_log_filename(log_id)
 
-        ulog = load_ulog_file(ulog_file_name)
-        px4_ulog = PX4ULog(ulog)
-        px4_ulog.add_roll_pitch_yaw()
+            db_handle = DatabaseULog.get_db_handle(get_db_filename())
+            dbulog_pk = None
+            with db_handle() as db:
+                cur = db.cursor()
+                cur.execute('select ULogId from Logs where Id = ?', [log_id])
+                dbulog_pk, = cur.fetchone()
 
+            if dbulog_pk is None:
+                raise KeyError(f'Found Log object of {log_id=} with ULogId set')
+            else:
+                ulog = DatabaseULog(db_handle, primary_key=dbulog_pk)
+                print(f'Found ULog with {dbulog_pk=} in database.')
+    except KeyError:
+        ulog = load_ulog_file(ulog_file_name)
+        print(f'Loading ULog {log_id} from file')
     except ULogException:
         error_message = ('A parsing error occured when trying to read the file - '
                          'the log is most likely corrupt.')
-    except:
-        print("Error loading file:", sys.exc_info()[0], sys.exc_info()[1])
-        error_message = 'An error occured when trying to read the file.'
 
+    px4_ulog = PX4ULog(ulog)
 
     print_timing("Data Loading", start_time)
     start_time = timer()

plot_app/pid_analysis.py
@@ -346,11 +346,11 @@ class Trace:
         return (average, np.sqrt(variance))
 
 
-def plot_pid_response(trace, data, plot_config, label='Rate'):
+def plot_pid_response(trace, ulog, plot_config, label='Rate'):
     """Plot PID response for one axis
 
     :param trace: Trace object
-    :param data: ULog.data_list
+    :param ulog: ULog
     """
 
     def _color_palette(hue, N=20):
@@ -374,7 +374,7 @@ def plot_pid_response(trace, data, plot_config, label='Rate'):
         return colors
 
 
-    data_plot = DataPlot(data, plot_config, 'sensor_combined',
+    data_plot = DataPlot(ulog, plot_config, 'sensor_combined',
                          y_axis_label='strength', x_axis_label='[s]',
                          title='Step Response for {:} {:}'.format(trace.name.capitalize(), label),
                          x_range=Range1d(0, trace.resplen),

plot_app/pid_analysis_plots.py
@@ -45,13 +45,12 @@ The analysis may take a while...
         'PID Analysis') + page_intro
 
     plots = []
-    data = ulog.data_list
     flight_mode_changes = get_flight_mode_changes(ulog)
     x_range_offset = (ulog.last_timestamp - ulog.start_timestamp) * 0.05
     x_range = Range1d(ulog.start_timestamp - x_range_offset, ulog.last_timestamp + x_range_offset)
 
     # COMPATIBILITY support for old logs
-    if any(elem.name == 'vehicle_angular_velocity' for elem in data):
+    if any(elem.name == 'vehicle_angular_velocity' for elem in ulog.data_list):
         rate_topic_name = 'vehicle_angular_velocity'
         rate_field_names = ['xyz[0]', 'xyz[1]', 'xyz[2]']
     else: # old
@@ -92,7 +91,7 @@ The analysis may take a while...
     for index, axis in enumerate(['roll', 'pitch', 'yaw']):
         axis_name = axis.capitalize()
         # rate
-        data_plot = DataPlot(data, plot_config, 'actuator_controls_0',
+        data_plot = DataPlot(ulog, plot_config, 'actuator_controls_0',
                              y_axis_label='[deg/s]', title=axis_name+' Angular Rate',
                              plot_height='small',
                              x_range=x_range)
@@ -149,7 +148,7 @@ The analysis may take a while...
                                      np.rad2deg(vehicle_rates_setpoint.data[axis]),
                                      gyro_time)
                 trace = Trace(axis, time_seconds, gyro_rate, setpoint, throttle)
-                plots.append(plot_pid_response(trace, ulog.data_list, plot_config).bokeh_plot)
+                plots.append(plot_pid_response(trace, ulog, plot_config).bokeh_plot)
             except Exception as e:
                 print(type(e), axis, ":", e)
                 div = Div(text="<p><b>Error</b>: PID analysis failed. Possible "
@@ -176,7 +175,7 @@ The analysis may take a while...
                                      np.rad2deg(vehicle_attitude_setpoint.data[axis+'_d']),
                                      attitude_time)
                 trace = Trace(axis, time_seconds, attitude_estimated, setpoint, throttle)
-                plots.append(plot_pid_response(trace, ulog.data_list, plot_config,
+                plots.append(plot_pid_response(trace, ulog, plot_config,
                                                'Angle').bokeh_plot)
             except Exception as e:
                 print(type(e), axis, ":", e)

testing_notebook.ipynb
@@ -103,7 +103,7 @@
    "outputs": [],
    "source": [
     "# plot raw acceleration sensor data\n",
-    "data_plot = DataPlot(data, plot_config, 'sensor_combined',\n",
+    "data_plot = DataPlot(ulog, plot_config, 'sensor_combined',\n",
     "        y_axis_label = '[m/s^2]', title = 'Raw Acceleration', plot_height = 'normal')\n",
     "data_plot.add_graph(['accelerometer_m_s2[0]', 'accelerometer_m_s2[1]', 'accelerometer_m_s2[2]'],\n",
     "                    colors3, ['x', 'y', 'z'], use_downsample=use_downsample)\n",
@@ -119,7 +119,7 @@
    "outputs": [],
    "source": [
     "# a more complex plot with multiple datasets\n",
-    "data_plot = DataPlot(data, plot_config, 'vehicle_magnetometer', y_start=0, title = 'Thrust and Magnetic Field',\n",
+    "data_plot = DataPlot(ulog, plot_config, 'vehicle_magnetometer', y_start=0, title = 'Thrust and Magnetic Field',\n",
     "                     plot_height='normal')\n",
     "data_plot.add_graph([lambda data: ('len_mag', \n",
     "    np.sqrt(data['magnetometer_ga[0]']**2 + data['magnetometer_ga[1]']**2 + data['magnetometer_ga[2]']**2))],\n",

tornado_handlers/upload.py
@@ -16,6 +16,7 @@ from tornado.ioloop import IOLoop
 
 from pyulog import ULog
 from pyulog.px4 import PX4ULog
+from pyulog.db import DatabaseULog
 
 # this is needed for the following imports
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../plot_app'))
@@ -207,6 +208,9 @@ class UploadHandler(TornadoRequestHandlerBase):
                 if source != 'CI':
                     ulog_file_name = get_log_filename(log_id)
                     ulog = load_ulog_file(ulog_file_name)
+                    db_handle = DatabaseULog.get_db_handle(get_db_filename())
+                    dbulog = DatabaseULog(db_handle, ulog=ulog)
+                    dbulog.save()
 
 
                 # put additional data into a DB

commit 73443defe8705be3c01d6059ec01c06e3d1fab13

Fix bug where DataPlot ignores topic_instance
This bug was introduced in 9d1f4cde15d503e76536832c9b05e3cc8d8e4764 (Use
DatabaseULog in plot_app).
Fixed by passing topic_instance on from the DataPlot constructor to _cur_dataset.

plot_app/plotting.py
@@ -566,7 +566,7 @@ class DataPlot:
                     plot_parameter_changes(self._p, self.plot_height,
                                            changed_params)
 
-            self._cur_dataset = ulog.get_dataset(data_name)
+            self._cur_dataset = ulog.get_dataset(data_name, multi_instance=topic_instance)
 
             if y_start is not None:
                 # make sure y axis starts at y_start. We do it by adding an invisible circle