1995beaf0eb24375bede30411ad002126a71b3ba
Herman Øie Kolden <herman@samfundet.no>
2022-12-13 14:21:37 +0100

    Add wind estimator plots

--- plot_app/configured_plots.py
+++ plot_app/configured_plots.py
@@ -516,6 +516,10 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
             data_plot.add_graph(['vel_m_s'], colors8[2:3], ['Ground Speed (from GPS)'])
             data_plot.change_dataset('tecs_status')
             data_plot.add_graph([true_airspeed_sp_fieldname], colors8[3:4], ['True Airspeed Setpoint'])
+            data_plot.change_dataset('wind')
+            data_plot.add_graph([lambda data: ('windspeed',
+                                np.sqrt(data['windspeed_north']**2 + data['windspeed_east']**2))],
+                                colors8[4:5], ['Estimated Wind'])
             plot_flight_modes_background(data_plot, flight_mode_changes, vtol_states)
 
             if data_plot.finalize() is not None: plots.append(data_plot)
