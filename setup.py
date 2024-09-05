6e21ede89f7fa17a8a3194b65eb9d6215bed18ad
Fredrik Bjørnland <bjornlandfredrik@gmail.com>
2024-09-05 08:46:47 +0200

    build: ci/cd pipelie to aws

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+__version__ = "0.0.0"
+
+setup(
+    name="Flight Review",
+    version=__version__,
+    author="Herman Kolden",
+    author_email="herman@aviant.no",
+    packages=[],
+    description="Log analysis tool for PX4 logs",
+    install_requires=[],
+)
